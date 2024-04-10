85e8b77bc9a4d60072975ae046ffd54752707fbf
Martin Accou <martin.accou@icloud.com>
2024-04-10 18:13:06 +0200

    reorder

--- labelizer/in_memory_db.py
+++
@@ -1,9 +0,0 @@
-from labelizer.model import LabelizedTriplet
-
-IN_MEMORY_DB: list[LabelizedTriplet] = [
-    LabelizedTriplet(0, 0, 0, 0),
-    LabelizedTriplet(1, 1, 1, 1),
-    LabelizedTriplet(2, 2, 2, 2),
-]
-
-IN_MEMORY_DB = {}
--- labelizer/routes.py
+++ labelizer/routes.py
@@ -1,17 +1,24 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 from starlette.responses import FileResponse
 
-from labelizer.core.database.database import get_db
+from labelizer.core.database.init_database import SessionLocal
 from labelizer.model import LabelizerTripletResponse, SelectedItemType
 
 router = APIRouter(tags=["Studies Management"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @router.get(
     "/images",
     summary="TODO",
     status_code=status.HTTP_200_OK,
 )
-async def get_image(user_id:str, image_id:str):
+async def get_image(user_id:str, image_id:str) -> FileResponse:
     return FileResponse(f'/images/{image_id}')
 
 @router.get(
@@ -19,11 +26,8 @@ async def get_image(user_id:str, image_id:str):
     summary="TODO",
     status_code=status.HTTP_200_OK,
 )
-async def make_labelizer_triplet(
+async def make_labelizer_triplet(Session = Depends(get_db)
 ) -> LabelizerTripletResponse:
-    with get_db() as session:
-        # result = get_triplet(session)
-        result = 0
 
     result = LabelizerTripletResponse("0", "0", "0", "0")
 
@@ -38,7 +42,7 @@ async def set_triplet_label(
     user_id:str,
     request_id:str,
     label:SelectedItemType,
-):
+) -> None:
     """
     TODO
     """
