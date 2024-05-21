24e942569f07080c9ccd0ceeb2319167ce00f0a2
Martin Accou <martin.accou@icloud.com>
2024-05-21 11:31:08 +0200

    add inheritance for schemas

--- labelizer/schemas.py
+++ labelizer/schemas.py
@@ -4,11 +4,8 @@ from pydantic import BaseModel
 
 from labelizer.types import SelectedItemType
 
-# This file contains the Pydantic models, while the models file contains the SQLAlchemy models
 
-
-class LabelizerTripletResponse(BaseModel):
-    id: int
+class TripletBase(BaseModel):
     reference_id: str
     reference_length: float
     left_id: str
@@ -20,52 +17,27 @@ class LabelizerTripletResponse(BaseModel):
         from_attributes = True
 
 
-class LabelizerValidationTripletResponse(BaseModel):
+class LabelizerTripletResponse(TripletBase):
     id: int
-    reference_id: str
-    reference_length: float
-    left_id: str
-    left_length: float
+
+
+class LabelizerValidationTripletResponse(TripletBase):
+    id: int
     left_encoder_id: str
-    right_id: str
-    right_length: float
     right_encoder_id: str
 
-    class Config:
-        from_attributes = True
 
-
-class LabelizedTriplet(BaseModel):
-    reference_id: str
-    reference_length: float
-    left_id: str
-    left_length: float
-    right_id: str
-    right_length: float
+class LabelizedTriplet(TripletBase):
     encoder_id: str
-    # By default, the label is None, meaning that the user has not selected any option
     label: SelectedItemType | None = None
-    # Same for the user_id
     user_id: str | None = None
 
-    class Config:
-        from_attributes = True
 
-
-class ValidationTriplet(BaseModel):
-    reference_id: str
-    reference_length: float
-    left_id: str
-    left_length: float
+class ValidationTriplet(TripletBase):
     left_encoder_id: str
-    right_id: str
-    right_length: float
     right_encoder_id: str
     label: SelectedItemType | None = None
     user_id: str | None = None
-
-    class Config:
-        from_attributes = True
 
 
 class TripletStats(BaseModel):
