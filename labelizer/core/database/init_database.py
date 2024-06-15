b0c3b5f4865a7e3d38b750fc4421111c8ca27cd0
Martin Accou <martin.accou@icloud.com>
2024-06-15 21:30:52 +0200

    successfully added the check for extension vector installed

--- labelizer/core/database/init_database.py
+++ labelizer/core/database/init_database.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine
+import logging
+
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 from labelizer.app_config import AppConfig
@@ -7,10 +9,27 @@ app_config = AppConfig()
 
 SQLALCHEMY_DATABASE_URL = app_config.db_url
 
+# For this logger, we have to specify the config otherwise we do not see anything logged
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
 )
+
+with engine.connect() as conn:
+    result = conn.execute(
+        text("SELECT extname FROM pg_extension WHERE extname = 'vector';"),
+    )
+    extension_name = (
+        result.scalar()
+    )  # Will return None if there is no row in the result
+    if extension_name == "vector":
+        logger.info("Extension 'vector' is installed")
+    else:
+        msg = "Extension 'vector' is not installed."
+        raise Exception(msg)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- labelizer/types.py
+++ labelizer/types.py
@@ -6,9 +6,3 @@ class SelectedItemType(str, Enum):
     RIGHT = "right"
     DONT_KNOW = "dont_know"
     # ? Add an option if the two left and right items tend to be the most similar, make it more complex for the next iteration
-
-
-class UploadStatus(str, Enum):
-    UPLOADED = "UPLOADED"
-    PROCESSING = "PROCESSING"
-    PROCESSED = "PROCESSED"
