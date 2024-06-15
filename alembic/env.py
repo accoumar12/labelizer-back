991aefa673cef012ec6207eaf7823b3006f7eed7
Martin Accou <martin.accou@icloud.com>
2024-06-15 13:25:23 +0200

    make the first migration, and using an env variable for the db_url

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
@@ -56,8 +57,12 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    # We retrieve the database URL from the environment variables here, as suggested by https://github.com/sqlalchemy/alembic/discussions/1149
+    configuration = config.get_section(config.config_ini_section)
+    configuration["sqlalchemy.url"] = os.environ["DB_URL"]
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
